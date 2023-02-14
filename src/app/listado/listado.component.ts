a3b575bad91e40b4f0bcfa983e4a78a89751a419
Javier Lete <jglete@eskura21.com>
2023-02-14 19:48:48 +0100

    Visualizar listado con datos de mockup

--- src/app/listado/listado.component.ts
+++ src/app/listado/listado.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../cliente';
+import { CLIENTES } from '../mock-clientes';
 
 @Component({
   selector: 'app-listado',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent {
+  clientes: Cliente[] = CLIENTES;
 
+  borrar(id: number): void {
+    alert(id);
+  }
 }
