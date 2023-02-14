967772faafa1ef6390b77a539fb24d63e52a67ff
Javier Lete <jglete@eskura21.com>
2023-02-14 20:16:25 +0100

    Doble binding con fecha

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../cliente';
 
 @Component({
   selector: 'app-formulario',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent {
+  cliente: Cliente = { id: 25, nombre: 'asdf', dni: '1234', fechaNacimiento: new Date() };
 
+  aceptar() {
+    console.log(this.cliente);
+  }
+
+  cambioFecha(fechaTexto: string) {
+    this.cliente.fechaNacimiento = new Date(fechaTexto);
+    console.log(this.cliente);
+  }
 }
