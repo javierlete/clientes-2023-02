221b6b5cd835b27d6d1c987f4d38484d942f8021
Javier Lete <jglete@eskura21.com>
2023-02-14 20:19:52 +0100

    Visualización del cliente concreto en editar

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Cliente } from '../cliente';
+import { CLIENTES } from '../mock-clientes';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent {
-  cliente: Cliente = { id: 25, nombre: 'asdf', dni: '1234', fechaNacimiento: new Date() };
+export class FormularioComponent implements OnInit {
+  cliente: Cliente = { id: 0, nombre: '', dni: '', fechaNacimiento: new Date() };
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id: number = Number(this.route.snapshot.paramMap.get('id'));
+    this.cliente = CLIENTES.find(cliente => cliente.id === id)!;
+  }
 
   aceptar() {
     console.log(this.cliente);
